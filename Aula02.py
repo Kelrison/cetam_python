9dcbd57c20e964df2162daed3a3988b037ad6a97
Kelrison Coelho <kelrison_coelho@hotmail.com>
2026-09-03 19:20:07 -0400

    Add files via upload

--- Aula02.py
+++ Aula02.py
@@ -5,4 +5,10 @@ altura = float(input("Informe sua altura:"))
 print ("O nome informado foi:", nome)
 print ("A idade informada foi:", idade)
 print ("A altura informada foi:", altura)
+print (f"A altura informada foi: {nome}")
 
+
+if idade >= 18:
+    print("Voto obrigatório")
+else:
+    print("Voto NÃO obrigatório")
