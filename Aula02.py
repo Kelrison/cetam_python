6a90910e979199e1ca443579b77ff543b1a96f0f
Kelrison Coelho <kelrison_coelho@hotmail.com>
2026-09-03 17:43:51 -0400

    Upload de aula 02 em py

---
+++ Aula02.py
@@ -0,0 +1,8 @@
+nome = input("Informe seu nome:")
+idade = int(input("Informe sua idade:"))
+altura = float(input("Informe sua altura:"))
+
+print ("O nome informado foi:", nome)
+print ("A idade informada foi:", idade)
+print ("A altura informada foi:", altura)
+
