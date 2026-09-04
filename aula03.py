da34aede1fb75fc2da36498470375c154d09bc07
Kelrison Coelho <kelrison_coelho@hotmail.com>
2026-09-04 19:43:24 -0400

    Add files via upload

--- aula03.py
+++ aula03.py
@@ -3,3 +3,7 @@ print(f"O numéro digitado foi:{numero1}")
 
 numero2 = float(numero1)
 print(f"O número conververtido para float é:{numero2}")
+
+#Exemplo de impressão de float com zeros 
+valor = 1.200
+print (f"o valor é: {valor:.3f}")
