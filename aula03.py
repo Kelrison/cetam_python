50acce90c956147c02b509cae56f04427596333b
Kelrison Coelho <kelrison_coelho@hotmail.com>
2026-09-04 19:49:34 -0400

    Add files via upload

--- aula03.py
+++ aula03.py
@@ -1,15 +1,9 @@
-# Exemplo de conversão Texto -> Float 
 numero1 = input("Digite o primeiro número:")
 print(f"O numéro digitado foi:{numero1}")
 
 numero2 = float(numero1)
 print(f"O número conververtido para float é:{numero2}")
 
-#Exemplo de If, Elif, Else
-idade = 14
-if idade >= 18:
-    print("Voto Obrigatório!")
-elif idade >= 16:
-    print("Voto Facultativo!")
-else:
-    print("Não vota")
+#Exemplo de impressão de float com zeros 
+valor = 1.200
+print (f"o valor é: {valor:.3f}")
