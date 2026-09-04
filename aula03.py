1a58bb99b856b293a142c9bb728ebc91558522c8
Kelrison <kelrison_coelho@hotmail.com>
2026-09-04 19:45:44 -0400

    Upload de arquivos

--- aula03.py
+++ aula03.py
@@ -3,3 +3,11 @@ print(f"O numéro digitado foi:{numero1}")
 
 numero2 = float(numero1)
 print(f"O número conververtido para float é:{numero2}")
+
+idade = 14
+if idade >= 18:
+    print("Voto Obrigatório!")
+elif idade >= 16:
+    print("Voto Facultativo!")
+else:
+    print("Não vota")
